1a9859eac6db0e13b213843fd9323f9b60541a04
Cedryna DUPEL <cedrynadupel@estiam.com>
2024-03-27 14:48:40 +0100

    complement

--- app.py
+++ app.py
@@ -33,6 +33,12 @@ def load_data(lag_days: int):
 
 df, daily_avg = load_data(LAG_N_DAYS)
 
+# Assigning colors to each day of the week
+colors = px.colors.qualitative.Set3
+
+# Ordering days of the week from Monday to Friday
+days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
 st.subheader("Line Chart of Numerical Data Over Time")
 numerical_column = col_donnees
 fig = px.line(df, x=col_date, y=col_donnees, title="Consommation en fonction du temps")
@@ -40,6 +46,10 @@ st.plotly_chart(fig)
 
 st.subheader("Average Consumption per Day of the Week")
 
-# Create a bar chart for daily average consumption
-fig_bar = px.bar(daily_avg, x='day_of_week', y=col_donnees, title='Moyenne de la consommation par jour de la semaine')
+# Sorting data by day of the week and reindexing
+daily_avg = daily_avg.set_index('day_of_week').reindex(days_order).reset_index()
+
+# Create a bar chart for daily average consumption with custom colors
+fig_bar = px.bar(daily_avg, x='day_of_week', y=col_donnees, title='Moyenne de la consommation par jour de la semaine',
+                 color='day_of_week', color_discrete_sequence=colors)
 st.plotly_chart(fig_bar)
